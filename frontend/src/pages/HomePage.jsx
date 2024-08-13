01f3d6ca27c23a861d5070b4261f18eb5732c7e6
Mohanad <mohannadnasar99@gmail.com>
2024-08-13 10:58:39 +0300

    Pushing new docker image

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -45,7 +45,7 @@ const HomePage = () => {
     <div className="homepage">
       <div className="welcome-section">
         <h1>Welcome to <span className='redText'>DH<span className='greyTextHero'>BW</span></span>-AppStore !!</h1>
-        <p>Empowering You with Effortless Cloud Applications Management.</p>
+        <p>Empowering You with Effortless Cloud Applications Management</p>
       </div>
 
       {error && <div className="error-message">{error}</div>}
